893ede3f3ee90921708541d00a692815b06ac6fc
Eng_AbdElRahmanAzzam <122513601+EngAbdElRahmanAzzam@users.noreply.github.com>
2025-02-24 23:42:32 +0200

    set protected route

--- Product-Builder Ui/src/routes/router.config.tsx
+++ Product-Builder Ui/src/routes/router.config.tsx
@@ -8,6 +8,8 @@ import BrandsPage from "../pages/main/brands";
 import UsersPage from "../pages/main/users";
 import CategoriesPage from "../pages/main/categories";
 import NotFoundPage from "../pages/errorHandling/notFoundPage";
+import ProtectedRoute from "../compontents/common/protectedRoute";
+import { isAuth } from "../authorization/isAuth";
 
 const errorElement = <ErrorPage />;
 
@@ -15,9 +17,17 @@ const errorElement = <ErrorPage />;
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
+            {/* handle home url */}
             <Route path="/home" element=<Navigate to="/"/> />
 
-            <Route path="/" element=<MainLayout/> errorElement={errorElement}>
+            {/* main pages => protected must auth */}
+            <Route path="/"  
+            element=
+                    <ProtectedRoute isAllowed={isAuth()} redirect="sign-in">
+                        element=<MainLayout/>
+                    </ProtectedRoute> errorElement={errorElement}
+            >
+
                 <Route index element=<HomePage/>  />
                 <Route path="products" element =<ProductsPage/> />
                 <Route path="catergories" element=<CategoriesPage/> />
@@ -25,8 +35,15 @@ export const router = createBrowserRouter(
                 <Route path="users" element=<UsersPage/> />
             </Route>
 
-            <Route path="sign-in" element=<SignInPage/> errorElement={errorElement}/>
+            {/* main pages => unprotected must unauth */}
+            <Route path="sign-in" 
+            element=
+                    <ProtectedRoute isAllowed={!isAuth()} redirect="/">
+                        <SignInPage/>
+                    </ProtectedRoute> errorElement={errorElement}
+            />
 
+            {/* handle wrong url */}
             <Route path="*" element=<NotFoundPage/> />
         </>
     )
